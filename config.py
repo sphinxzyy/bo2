08e531fb36a993248160a00663318f05f7dace7b
sphinxzyy <115400469+sphinxzyy@users.noreply.github.com>
2022-12-04 09:06:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://frkmwxlxjeoauu:6a6010ac3b3e904da0c1848d0fe03ca8076b5467e786a3393890b663c5bb5a7f@ec2-54-160-73-231.compute-1.amazonaws.com:5432/d6pf7n1t22r20j")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001588507841"))
